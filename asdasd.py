824ba387ed860b664fe9e772242922887ea589e0
BMoroziuk <bogdan.moroziuk@mindy-support.com>
2019-11-19 16:52:48 +0200

    ddd

--- asdasd.py
+++ asdasd.py
@@ -18,20 +18,24 @@ cot = ['EURUSD', 'USDCHF', 'GBPUSD', 'USDJPY', 'EURJPY', 'AUDUSD', 'NZDUSD',
     'EURGBP', 'USDCAD',   'EURCHF',  'EURAUD',
 
      'USDSGD'];
-
+supersum = 0
 for co in cot:
-    URL = f"https://my.liteforex.com/ru/chart/history?symbol={co}&resolution=60&from={time.time()-365*86400}&to={time.time()-0*86400}"
+    URL = f"https://my.liteforex.com/ru/chart/history?symbol={co}&resolution=1D&from={time.time()-1000*86400}&to={time.time()-0*86400}"
     r = requests.get(url=URL)
     data = r.json()
     csvdata = []
-    for i in range(4, len(data['c'])-1):
-        csvdata.append([data['l'][i-3], data['o'][i-3], data['c'][i-3], data['h'][i-3], data['l'][i-2], data['o'][i-2], data['c'][i-2], data['h'][i-2], data['l'][i-1], data['o'][i-1], data['c'][i-1], data['h'][i-1], data['l'][i], data['o'][i], data['c'][i], data['h'][i]])
-        if data['c'][i]>data['c'][i-1]:
-
-            csvdata[len(csvdata) - 1].append('Up')
-        elif data['c'][i]<data['c'][i-1]:
-
-            csvdata[len(csvdata) - 1].append('Down')
+    for i in range(8, len(data['c'])-1):
+        csvdata.append([data['l'][i-7], data['o'][i-7], data['c'][i-7], data['h'][i-7],data['l'][i-6], data['o'][i-6], data['c'][i-6], data['h'][i-6],data['l'][i-5], data['o'][i-5], data['c'][i-5], data['h'][i-5],data['l'][i-4], data['o'][i-4], data['c'][i-4], data['h'][i-4],data['l'][i-3], data['o'][i-3], data['c'][i-3], data['h'][i-3], data['l'][i-2], data['o'][i-2], data['c'][i-2], data['h'][i-2], data['l'][i-1], data['o'][i-1], data['c'][i-1], data['h'][i-1], data['l'][i], data['o'][i], data['c'][i], data['h'][i]])
+        if (data['c'][i]-data['c'][i-1] > 0.001 and 'JPY' not in co) or (data['c'][i]-data['c'][i-1] > 0.1 and 'JPY' in co):
+            if co != 'AUDUSD' and co != 'EURUSD':
+                csvdata[len(csvdata) - 1].append('Down')
+            else:
+                csvdata[len(csvdata) - 1].append('Up')
+        elif (data['c'][i]-data['c'][i-1]< -0.1 and 'JPY' not in co) or (data['c'][i]-data['c'][i-1]< -0.1 and 'JPY' in co):
+            if co != 'AUDUSD' and co != 'EURUSD':
+                csvdata[len(csvdata) - 1].append('Up')
+            else:
+                csvdata[len(csvdata) - 1].append('Down')
         else:
             csvdata[len(csvdata) - 1].append('Rovno')
 
@@ -45,13 +49,13 @@ for co in cot:
 
     # Load dataset
     url = "test.csv"
-    names = ['l0', 'o0', 'c0', 'h0','l1', 'o1', 'c1', 'h1', 'l2', 'o2', 'c2', 'h2', 'l3', 'o3', 'c3', 'h3', 'class']
+    names = ['l-3', 'o-3', 'c-3', 'h-3','l-2', 'o-2', 'c-2', 'h-2','l-1', 'o-1', 'c-1', 'h-1','l', 'o', 'c', 'h','l0', 'o0', 'c0', 'h0','l1', 'o1', 'c1', 'h1', 'l2', 'o2', 'c2', 'h2', 'l3', 'o3', 'c3', 'h3', 'class']
     dataset = read_csv(url, names=names)
     # Split-out validation dataset
     array = dataset.values
-    X = array[:,0:16]
+    X = array[:,0:32]
 
-    y = array[:,16]
+    y = array[:,32]
     X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
 
     # Spot Check Algorithms
@@ -86,9 +90,64 @@ for co in cot:
     #
     LDA = LinearDiscriminantAnalysis()
     LDA.fit(X, y)
+    summ = 0
     print(co)
-    print(LDA.predict([[data['l'][len(data['c'])-5], data['o'][len(data['c'])-5], data['c'][len(data['c'])-5], data['h'][len(data['c'])-5], data['l'][len(data['c'])-4], data['o'][len(data['c'])-4], data['c'][len(data['c'])-4], data['h'][len(data['c'])-4], data['l'][len(data['c'])-3], data['o'][len(data['c'])-3], data['c'][len(data['c'])-3], data['h'][len(data['c'])-3],data['l'][len(data['c'])-2], data['o'][len(data['c'])-2], data['c'][len(data['c'])-2], data['h'][len(data['c'])-2]]])[0])
+    i = 0
+    print(LDA.predict([[data['l'][len(data['c']) - 9 + i], data['o'][len(data['c']) - 9 + i],
+                          data['c'][len(data['c']) - 9 + i], data['h'][len(data['c']) - 9 + i],data['l'][len(data['c']) - 8 + i], data['o'][len(data['c']) - 8 + i],
+                          data['c'][len(data['c']) - 8 + i], data['h'][len(data['c']) - 8 + i],data['l'][len(data['c']) - 7 + i], data['o'][len(data['c']) - 7 + i],
+                          data['c'][len(data['c']) - 7 + i], data['h'][len(data['c']) - 7 + i],data['l'][len(data['c']) - 6 + i], data['o'][len(data['c']) - 6 + i],
+                          data['c'][len(data['c']) - 6 + i], data['h'][len(data['c']) - 6 + i],data['l'][len(data['c']) - 5 + i], data['o'][len(data['c']) - 5 + i],
+                          data['c'][len(data['c']) - 5 + i], data['h'][len(data['c']) - 5 + i],
+                          data['l'][len(data['c']) - 4 + i], data['o'][len(data['c']) - 4 + i],
+                          data['c'][len(data['c']) - 4 + i], data['h'][len(data['c']) - 4 + i],
+                          data['l'][len(data['c']) - 3 + i], data['o'][len(data['c']) - 3 + i],
+                          data['c'][len(data['c']) - 3 + i], data['h'][len(data['c']) - 3 + i],
+                          data['l'][len(data['c']) - 2 + i], data['o'][len(data['c']) - 2 + i],
+                          data['c'][len(data['c']) - 2 + i], data['h'][len(data['c']) - 2 + i]]])[0])
     print()
+#     for i in range(0, -240, -1):
+#
+#         if (LDA.predict([[data['l'][len(data['c']) - 9 + i], data['o'][len(data['c']) - 9 + i],
+#                           data['c'][len(data['c']) - 9 + i], data['h'][len(data['c']) - 9 + i],data['l'][len(data['c']) - 8 + i], data['o'][len(data['c']) - 8 + i],
+#                           data['c'][len(data['c']) - 8 + i], data['h'][len(data['c']) - 8 + i],data['l'][len(data['c']) - 7 + i], data['o'][len(data['c']) - 7 + i],
+#                           data['c'][len(data['c']) - 7 + i], data['h'][len(data['c']) - 7 + i],data['l'][len(data['c']) - 6 + i], data['o'][len(data['c']) - 6 + i],
+#                           data['c'][len(data['c']) - 6 + i], data['h'][len(data['c']) - 6 + i],data['l'][len(data['c']) - 5 + i], data['o'][len(data['c']) - 5 + i],
+#                           data['c'][len(data['c']) - 5 + i], data['h'][len(data['c']) - 5 + i],
+#                           data['l'][len(data['c']) - 4 + i], data['o'][len(data['c']) - 4 + i],
+#                           data['c'][len(data['c']) - 4 + i], data['h'][len(data['c']) - 4 + i],
+#                           data['l'][len(data['c']) - 3 + i], data['o'][len(data['c']) - 3 + i],
+#                           data['c'][len(data['c']) - 3 + i], data['h'][len(data['c']) - 3 + i],
+#                           data['l'][len(data['c']) - 2 + i], data['o'][len(data['c']) - 2 + i],
+#                           data['c'][len(data['c']) - 2 + i], data['h'][len(data['c']) - 2 + i]]])[0]) == 'Up':
+#             # print(data['c'][len(data['c']) - 1 + i] - data['c'][len(data['c']) - 2 + i])
+#             if 'JPY' in co:
+#                 summ += (data['c'][len(data['c']) - 1 + i] - data['c'][len(data['c']) - 2 + i])/100
+#             else:
+#                 summ += data['c'][len(data['c']) - 1 + i] - data['c'][len(data['c']) - 2 + i]
+#
+#         elif (LDA.predict([[data['l'][len(data['c']) - 9 + i], data['o'][len(data['c']) - 9 + i],
+#                             data['c'][len(data['c']) - 9 + i], data['h'][len(data['c']) - 9 + i],data['l'][len(data['c']) - 8 + i], data['o'][len(data['c']) - 8 + i],
+#                             data['c'][len(data['c']) - 8 + i], data['h'][len(data['c']) - 8 + i],data['l'][len(data['c']) - 7 + i], data['o'][len(data['c']) - 7 + i],
+#                             data['c'][len(data['c']) - 7 + i], data['h'][len(data['c']) - 7 + i],data['l'][len(data['c']) - 6 + i], data['o'][len(data['c']) - 6 + i],
+#                             data['c'][len(data['c']) - 6 + i], data['h'][len(data['c']) - 6 + i],data['l'][len(data['c']) - 5 + i], data['o'][len(data['c']) - 5 + i],
+#                             data['c'][len(data['c']) - 5 + i], data['h'][len(data['c']) - 5 + i],
+#                             data['l'][len(data['c']) - 4 + i], data['o'][len(data['c']) - 4 + i],
+#                             data['c'][len(data['c']) - 4 + i], data['h'][len(data['c']) - 4 + i],
+#                             data['l'][len(data['c']) - 3 + i], data['o'][len(data['c']) - 3 + i],
+#                             data['c'][len(data['c']) - 3 + i], data['h'][len(data['c']) - 3 + i],
+#                             data['l'][len(data['c']) - 2 + i], data['o'][len(data['c']) - 2 + i],
+#                             data['c'][len(data['c']) - 2 + i], data['h'][len(data['c']) - 2 + i]]])[0]) == 'Down':
+#             # print(data['c'][len(data['c']) - 2 + i] - data['c'][len(data['c']) - 1 + i])
+#             if 'JPY' in co:
+#                 summ += (data['c'][len(data['c']) - 2 + i] - data['c'][len(data['c']) - 1 + i])/100
+#             else:
+#                 summ += data['c'][len(data['c']) - 2 + i] - data['c'][len(data['c']) - 1 + i]
+#
+#     print(summ)
+#     supersum += summ
+#     print()
+# print(supersum)
     # KNN = KNeighborsClassifier()
     # KNN.fit(X, y)
     # print('KNN', KNN.predict([[data['o'][len(data['c'])-2], data['l'][len(data['c'])-2], data['h'][len(data['c'])-2], data['c'][len(data['c'])-2]]])[0])
